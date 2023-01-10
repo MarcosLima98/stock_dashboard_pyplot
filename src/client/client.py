5e59fabdc6a1276ef123d79da94ce445fc716115
MarcosLima98 <marcoslimadeoliveira98@gmail.com>
2023-01-10 20:59:52 -0300

    MARCOS WUZ HERE

---
+++ src/client/client.py
@@ -0,0 +1,17 @@
+
+
+
+class client:
+    def __init__(self) -> None:
+        pass
+
+
+class stock:
+    def __init__(self) -> None:
+        pass
+
+    def set_stock(self):
+        pass
+
+    def get_stock():
+        pass
